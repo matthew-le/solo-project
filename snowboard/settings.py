bd7864d2be59fdeff1a7b4ab82b32f52fc18d892
Matthew Le <matthewthaile@gmail.com>
2021-12-17 01:38:11 -0800

    install decouple

--- snowboard/settings.py
+++ snowboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-from decouple import config
 
 SECRET_KEY = config("SECRET_KEY")
 
